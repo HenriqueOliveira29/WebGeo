5b8f3590d41cb32d8958c2249e8f54dd6f9db361
HenriqueOliveira29 <henrique.olive29@gmail.com>
2024-05-05 20:01:00 +0100

    create controller

--- WebGeoAPI/Controllers/OrderController.cs
+++ WebGeoAPI/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebGeo.BLL.Services;
+using WebGeoInfrastructure.DTOs.Order;
 using WebGeoInfrastructure.Entities;
+using WebGeoInfrastructure.Helpers;
 
 namespace WebGeoAPI.Controllers
 {
@@ -18,6 +20,13 @@ namespace WebGeoAPI.Controllers
         public async Task<List<Order>> GetOrders()
         {
             return await _orderService.GetOrders();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<MessagingHelper> Create(CreateOrderDTO createOrder)
+        {
+            return await _orderService.CreateOrder(createOrder);
         }
     }
 }
